3f17dc2db65b890723f446609987bf97f38389ac
Kevin Hoff <hoffkm@gmail.com>
2024-03-19 22:22:46 -0400

    Adjust vite build settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    minify: 'esbuild'
+  },
+  base: './'
 })
